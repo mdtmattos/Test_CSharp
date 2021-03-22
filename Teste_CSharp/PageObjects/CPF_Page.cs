300d58825fbfa620a10b60022a0ead60aa026aa8
Murilo Dellano Theodoro de Mattos <murilo.mattos@actminds.com>
2021-03-22 18:19:00 -0300

    Atualização projeto de teste

---
+++ Teste_CSharp/PageObjects/CPF_Page.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste_CSharp.PageObjects
+{
+    class CPF_Page
+    {
+        private RemoteWebDriver _driver;
+     
+        public CPF_Page(RemoteWebDriver driver) => _driver = driver;
+        IWebElement BtnValidadorCpf => _driver.FindElementByXPath("//body/div[1]/div[1]/div[2]/div[5]/div[1]/div[6]/a[1]");
+        IWebElement BtnValidarCPF => _driver.FindElementById("gerar");
+        IWebElement InserirCpf => _driver.FindElementById("cpf");
+        IWebElement MsgCPFValido => _driver.FindElementById("resposta1");
+
+        public void ClicarBtnValidarCPF()
+        {
+            BtnValidarCPF.Click();
+        }
+
+        public void ClicarBtnValidadorCPF()
+        {
+            BtnValidadorCpf.Click();
+        }
+        public void ValidarTituloPaginaCPF()
+        {
+            Assert.AreEqual("Validar CPF", _driver.FindElement(By.XPath("//h3[contains(text(),'Validar CPF')]")).Text);
+        }
+
+        public void InserirCPFInvalido()
+        {
+            InserirCpf.SendKeys(ConfigurationManager.AppSettings["cpfValido"]);
+        }
+        public void ValidarMensagemCPFinvalido()
+        {
+            Assert.AreEqual("O CPF é inválido", _driver.FindElement(By.Id("resposta2")).Text);
+        }
+        public void InserirCPFValido()
+        {
+            InserirCpf.SendKeys(ConfigurationManager.AppSettings["cpfValido"]);
+        }
+        public void ValidarMensagemCPFValido()
+        {
+            Assert.IsTrue(MsgCPFValido.Displayed);
+        }
+    }
+}
---
+++ Teste_CSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Teste_CSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Teste_CSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6937e149-0e1b-44f6-83b2-a26fceb95efb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
